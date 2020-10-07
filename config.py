27511cc26905bea28222c43b9365a56048a3d531
Genetical <18619788+Genetical@users.noreply.github.com>
2020-10-07 11:35:08 +0100

    Revert "Added GitHub Functionality"
    This reverts commit 8802b2fc7bdaf459ed182edea638f0e9e1a360de.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class bot:
-    token = os.environ["BOT_TOKEN"]
-    prefix = "!"
-    description = "Bot for the F4MP discord server."
-    initial_extensions = ["cogs.troubleshooter", "cogs.GithubManager"]
