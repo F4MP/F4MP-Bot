498a41d1b6c12a11491357d76fa1397fd5d601c7
Genetical <18619788+Genetical@users.noreply.github.com>
2020-10-07 11:35:02 +0100

    Revert "Update config.py"
    This reverts commit da948b10d66ed119962d860fe5f211d7af2761df.

--- F4MPBot/config.py
+++ F4MPBot/config.py
@@ -5,4 +5,4 @@ class bot:
     token = os.environ["BOT_TOKEN"]
     prefix = "!"
     description = "Bot for the F4MP discord server."
-    initial_extensions =  ["cogs.troubleshooter", "cogs.GithubManager"]
+    initial_extensions = ["cogs.troubleshooter"]
